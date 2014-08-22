4b74c9806f2e0f43d243720057db9fb9d094d9fa
luis <luis@netbook.(none)>
2014-08-22 18:00:16 +0200

    Modify cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { # x input matrix
+  m <- NULL			# init m to null
+				        # m will be the inverse
+  set <- function(y) {   # set function
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x 		# return value of input matrix
+  setsolve <- function(solve) m <<- solve # called by cacheSolve the first time
+  getsolve <- function() m	# return the cache value
+  list(set = set, get = get,	# list of accessed methods of the object
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {# x input matrix
+  m <- x$getsolve()		# get the solve value of the object x
+  if(!is.null(m)) {		# valued is cached
+    message("getting cached data") # print msg on screen
+    return(m)			# return inverse 
+  }
+  data <- x$get()		# calculate the inverse
+  m <- solve(data, ...)
+  x$setsolve(m)			# store te value
+  m				# return the inverse
+        
 }
